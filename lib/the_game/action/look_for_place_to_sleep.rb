059ce8ca6e54fba603ccb572449552cd95804f1d
Killa <l.strzebinczyk@gorailsgo.com>
2014-09-24 22:51:26 +0200

    more go to refactors

--- lib/the_game/action/look_for_place_to_sleep.rb
+++ lib/the_game/action/look_for_place_to_sleep.rb
@@ -7,18 +7,9 @@ class TheGame
 
       def perform(person, map, time_in_minutes)
         safe_place_to_sleep = TheGame::Settlement.instance.safe_place_to_sleep
-
-        if person.distance_to(safe_place_to_sleep) < 2.0
-          # assume that person needs a safe place to sleep
-          # for now, the only option is the fireplace
-
-          person.action = Action::Sleep.create(safe_place_to_sleep)
-        else
-          person.go_to(safe_place_to_sleep)
-        end
+        job = GoTo.create(safe_place_to_sleep).then(Sleep.create(safe_place_to_sleep))
+        person.action = job
       end
-
-      private
     end
   end
 end
