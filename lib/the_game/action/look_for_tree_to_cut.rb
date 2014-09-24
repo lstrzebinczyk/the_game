d4f89201f28060823447a95db8de2861955d12a7
Killa <l.strzebinczyk@gorailsgo.com>
2014-09-24 22:52:37 +0200

    go_to fixes done

--- lib/the_game/action/look_for_tree_to_cut.rb
+++ lib/the_game/action/look_for_tree_to_cut.rb
@@ -15,23 +15,13 @@ class TheGame
       end
 
       def perform(person, map, time_in_minutes)
-        # if person.has?(:axe)
-          closest = map.find_closest_to(person) do |tile|
-            tile.has_tree?
-          end
+        closest = map.find_closest_to(person) do |tile|
+          tile.has_tree?
+        end
 
-          if closest
-            person.action = Action::CutTree.create(closest)
-          end
-        # else
-        #   stash_tile = TheGame::Settlement.instance.stash_tile
-        #   if person.distance_to(stash_tile) < 2.0
-        #     axe = TheGame::Settlement.instance.stash.get(:axe)
-        #     person.inventory.add(axe)
-        #   else
-        #     person.go_to(stash_tile)
-        #   end
-        # end
+        if closest
+          person.action = Action::CutTree.create(closest)
+        end
       end
     end
   end
